b4a226136b42d2aca200618d3a841c4edc673695
baichen111 <baichen7788@gmail.com>
2021-12-29 10:16:38 +0800

    day5

---
+++ Day5/part1.py
@@ -0,0 +1,11 @@
+# -*- coding = utf-8 -*-
+# @time:12/29/21 10:06 AM
+# Author:Bai Chen
+# @File:part1.py
+# @Software:PyCharm
+
+
+if __name__ == '__main__':
+    with open("part1inputs.txt") as f:
+        data = f.read().strip().split()
+    print(data)
