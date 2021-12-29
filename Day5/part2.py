353c0d4746d78efc450f39a3f2745f83cff293e3
baichen111 <baichen7788@gmail.com>
2021-12-29 20:17:21 +0800

    day5

--- Day5/part2.py
+++ Day5/part2.py
@@ -1,12 +1,17 @@
 # -*- coding = utf-8 -*-
-# @time:12/29/21 10:06 AM
+# @time:12/29/21 12:41 PM
 # Author:Bai Chen
-# @File:part1.py
+# @File:part2.py
 # @Software:PyCharm
 
 
+def lin(x1,y1,x2,y2):
+    a = (y2 - y1) / (x2 - x1)
+    b = y1 - a*x1
+    return a,b
+
 if __name__ == '__main__':
-    with open("part1inputs.txt") as f:
+    with open("part2inputs.txt") as f:
         data = f.read().strip().split()
 
     ls = list(filter(lambda x: x != "->"  ,data))
@@ -22,8 +27,8 @@ if __name__ == '__main__':
         x1 = pair[0][0]; y1 = pair[0][1]; x2 = pair[1][0]; y2 = pair[1][1]
         min_y = min(y1, y2)
         max_y = max(y1, y2)
-        min_x = min(x1,x2)
-        max_x = max(x1,x2)
+        min_x = min(x1, x2)
+        max_x = max(x1, x2)
         if x1 == x2:
             for v in range(min_y,max_y + 1):
                 k = (x1,v)
@@ -36,19 +41,17 @@ if __name__ == '__main__':
                 if k not in d:
                     d[k] = 0
                 d[k] += 1
+        elif max_x - min_x == max_y - min_y:
+            a, b = lin(x1,y1,x2,y2)          #linear equation
+            for x in range(min_x,max_x + 1):
+                y = x*a + b
+                k = (x,y)
+                if k not in d:
+                    d[k] = 0
+                d[k] += 1
     s = 0
     for k,v in d.items():
         if v >= 2:
             s += 1
 
-    print(s)  #5576 final answer
-
-
-
-
-
-
-
-
-
-
+    print(s)   # 18144 final answer
